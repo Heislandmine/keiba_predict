766348ad1b61d06ae8a1d27e89fce4cc25dc4741
heislandmine <heislandmine@protonmail.com>
2020-12-28 14:51:08 +0900

    get_dataを辞書を返すように変更

--- test.py
+++ test.py
@@ -1,6 +1,5 @@
 from src.core.get_data import get_race_urls, get_race_data
 
-# race_urls = get_race_urls("20201226")
-
-res = get_race_data("https://race.netkeiba.com/race/result.html?race_id=202006050704")
-print(res)
+race_urls = get_race_urls("20201226")
+print(race_urls)
+# res = get_race_data("https://race.netkeiba.com/race/result.html?race_id=202006050704")
