efe859ac3d198093432b0b4fdb60d8884a28c685
heislandmine <heislandmine@protonmail.com>
2020-12-30 11:06:33 +0900

    ファイルの入出力を実装

--- add_header.py
+++ add_header.py
@@ -1,12 +1,20 @@
 """レースデータCSVに不足しているレース名、地面、距離の3つのヘッダーを追加するスクリプト"""
 import pathlib
+import sys
 
-p = pathlib.Path("./")
+root_dir = sys.argv[1]
+save_dir = sys.argv[2]
+
+p = pathlib.Path(root_dir)
 
 for path in p.glob("*.csv"):
     with open(path, "r", encoding="utf-8", newline="") as f:
         s = f.readlines()
-    with open("tst.csv", "w", encoding="utf-8", newline="") as f:
+
+    file_name = path.name
+    save_path = save_dir + file_name
+
+    with open(save_path, "w", encoding="utf-8", newline="") as f:
         for i, line in enumerate(s):
             # line = line.strip()
             if i == 0:
